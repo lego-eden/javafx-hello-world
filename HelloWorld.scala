bd9d09d08e4e1ba1346ff32e2c2254e0690e6f62
LegoEden <hampus.eden@gmail.com>
2023-11-28 10:30:06 +0100

    Initialized project

---
+++ HelloWorld.scala
@@ -0,0 +1,32 @@
+package helloworld
+
+import javafx.application.Application
+import javafx.stage.Stage
+import javafx.scene.control.Button
+import javafx.event.EventHandler
+import javafx.event.ActionEvent
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
+
+class HelloWorld extends Application:
+  def start(primaryStage: Stage): Unit = 
+    val btn = new Button:
+      setText("Say 'Hello World!'")
+
+      setOnAction(new EventHandler[ActionEvent]:
+        def handle(event: ActionEvent): Unit =
+          println("Hello World!")
+      )
+
+    val root = new StackPane:
+      getChildren.add(btn)
+      
+    val scene = new Scene(root, 300, 250)
+    
+    primaryStage.setTitle("Hello World!")
+    primaryStage.setScene(scene)
+    primaryStage.show()
+
+object HelloWorld:
+  def launch(args: String*): Unit =
+    Application.launch(classOf[HelloWorld], args*)
---
+++ main.scala
@@ -0,0 +1,5 @@
+package helloworld
+
+@main
+def main(args: String*): Unit =
+  HelloWorld.launch(args*)
